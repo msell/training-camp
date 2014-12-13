1bd39b92f98af654c90ecec6cb8bed17d19ca2d7
Matt Sell <mwsell@gmail.com>
2014-12-13 17:36:19 -0600

    bring in jwtAuth middleware

---
+++ api/api/policies/jwtAuth.js
@@ -0,0 +1,19 @@
+var jwt = require('jwt-simple');
+
+module.exports = function(req,res,next){
+    if(!req.headers || !req.headers.authorization)
+        return res.status(401).send({
+            message: 'You are not authorized'
+        });
+        
+    var token = req.headers.authorization.split(' ')[1];
+    var payload = jwt.decode(token, "shhh..");
+
+    if (!payload.sub) {
+        res.status(401).send({
+            message: 'Authentication failed'
+        });
+    }
+    
+    next();
+}
