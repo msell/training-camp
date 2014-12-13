eaf32533f3c4f46c7cee190ad1dc0523ae373f57
Matt Sell <mwsell@gmail.com>
2014-12-13 15:47:30 -0600

    basic sails

---
+++ api/api/controllers/LeagueController.js
@@ -0,0 +1,11 @@
+/**
+ * LeagueController
+ *
+ * @description :: Server-side logic for managing leagues
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
